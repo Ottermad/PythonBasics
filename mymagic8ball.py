29b4a4aa23affc5481fdfb29adf5026fd8492ac4
Charlie Thomas <charlie.thomas@attwoodthomas.net>
2014-07-11 21:32:26 +0100

    At CodeBox 3.3 p35

---
+++ mymagic8ball.py
@@ -0,0 +1,17 @@
+# MyMagic8Ball
+
+import random
+
+# write answers
+ans1 = "Go for it"
+ans2 = "No way, Jose!"
+ans3 = "I'm not sure. Ask me again."
+ans4 = "Fear of the unkown is what imprisons us."
+ans5 = "It would be madness to do that."
+ans6 = "Only you can save mankind!"
+ans7 = "Makes no difference to me, do or don't - whatever"
+ans8 = "Yes, I think on balance that is the right choice"
+
+print("Welcome to MyMagic8Ball!")
+
+# get the user's question
