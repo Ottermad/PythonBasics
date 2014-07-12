e4a452b5e1e889bf2c0b2b9cb2826053009f0a33
Ottermad <ClaireThomas@ATLTTSC855.AttwoodThomas.local>
2014-07-12 11:14:18 +0100

    Finished standard MyMagic8Ball

--- mymagic8ball.py
+++ mymagic8ball.py
@@ -15,3 +15,30 @@ ans8 = "Yes, I think on balance that is the right choice"
 print("Welcome to MyMagic8Ball!")
 
 # get the user's question
+question = input("Ask me for advice then press RETURN to shake me.\n")
+print("shaking ...\n" * 4)
+
+# use the randint() function to select the correct answer
+choice = random.randint(1, 8)
+
+if choice == 1:
+    answer = ans1
+elif choice == 2:
+    answer = ans2
+elif choice == 3:
+    answer = ans3
+elif choice == 4:
+    answer = ans4
+elif choice == 5:
+    answer = ans5
+elif choice == 6:
+    answer = ans6
+elif choice == 7:
+    answer = ans7
+else:
+    answer = ans8
+
+# print the answer to the screen
+print(answer)
+
+input("\n\nPress the RETURN key to finish.")
